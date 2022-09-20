4b30fb845f3b6e0ae71d338f537f831fdf3d2b3b
hcbarros <48363350+hcbarros@users.noreply.github.com>
2022-09-20 19:38:41 -0300

    alterando permissões

--- src/main/java/br/com/clinicavetgama/ClinicaVetGamaApplication.java
+++ src/main/java/br/com/clinicavetgama/ClinicaVetGamaApplication.java
@@ -2,12 +2,30 @@ package br.com.clinicavetgama;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ClinicaVetGamaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClinicaVetGamaApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						//.allowedOrigins("http://localhost:4200")
+						.allowedOrigins("*")
+						//.allowedMethods("PUT", "DELETE", "GET", "POST");
+						.allowedMethods("*")
+						.allowedHeaders("*");
+			}
+		};
 	}
 
 }
